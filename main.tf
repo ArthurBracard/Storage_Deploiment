1a72ac8da25eef742b8f7674f90d67b2bc9d78aa
ArthurBracard <99019915+ArthurBracard@users.noreply.github.com>
2022-02-23 09:23:13 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
 
   module "azure_storage" {
     source                 = "git@ssh.dev.azure.com:v3/vikwefight/terraform-modules/azure_storage_account"
-    name                   = var.name
+    name                   = "jamfrepo"
     subnet_datas           = { id = null }
     resource_group_datas   = data.azurerm_resource_group.storage
     storage_account_config = var.storage_config
